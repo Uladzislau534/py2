51a9bc8e711dd2e3a8d904ae3e6468246a9b09c7
Uladzislau Balan <vladislav.balan96@gmail.com>
2024-08-18 19:47:43 +0300

    3_end

--- PythonApplication3/PythonApplication3.py
+++ PythonApplication3/PythonApplication3.py
@@ -1,1 +1,17 @@
 
+# Ввод списка
+numbers = [1, 4, -3, 0, 10]
+
+# Сортировка вставками
+for i in range(1, len(numbers)):
+    key = numbers[i]
+    j = i - 1
+    # Перемещаем элементы, которые больше key, на одну позицию вперед
+    while j >= 0 and numbers[j] > key:
+        numbers[j + 1] = numbers[j]
+        j -= 1
+    # Вставляем key в правильную позицию
+    numbers[j + 1] = key
+
+# Вывод отсортированного списка
+print("Отсортированный список:", numbers)
