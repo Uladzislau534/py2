5012ced8f61f5acd58204307615049a36e19ed29
Uladzislau Balan <vladislav.balan96@gmail.com>
2024-08-18 19:37:41 +0300

    2_end

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,1 +1,20 @@
 
+num_of_cards = int(input("Количество видеокарт: "))
+
+# Ввод списка видеокарт
+cards = []
+for i in range(num_of_cards):
+    card = int(input(f"Видеокарта {i + 1}: "))
+    cards.append(card)
+
+# Вывод старого списка видеокарт
+print(f"Старый список видеокарт: {cards}")
+
+# Нахождение максимального значения
+max_card = max(cards)
+
+# Создание нового списка без максимальных значений
+new_cards = [card for card in cards if card != max_card]
+
+# Вывод нового списка видеокарт
+print(f"Новый список видеокарт: {new_cards}")
